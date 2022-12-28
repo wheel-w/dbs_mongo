f6da63f09dd4871bd1d2ff26a6f8e36c551b85c8
wheelwang <wheelwang@tencent.com>
2022-12-28 10:17:27 +0800

    minor: mongo instance add permissions

--- manager/views/mongo_instance.py
+++ manager/views/mongo_instance.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from manager.models import MongoInstance
+from manager.permissions.mongo_instance_permission import MongoInstancePermission
 from manager.serializers.mongo_instance_serializer import (
     MongoInstanceLoginListSerializer,
     MongoInstanceSerializer,
@@ -11,10 +12,11 @@ from manager.serializers.mongo_instance_serializer import (
 class MongoInstanceViewSet(ModelViewSet):
     queryset = MongoInstance.objects.all()
     serializer_class = MongoInstanceSerializer
+    permission_classes = [MongoInstancePermission]
     http_method_names = ["get", "post", "patch", "delete", "head", "options", "trace"]
 
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset()).filter(is_show=True)
+        queryset = self.filter_queryset(self.get_queryset()).filter(is_show=True, dbs_user_rtx=request.user.username)
 
         serializer = MongoInstanceLoginListSerializer(queryset, many=True)
         return Response(serializer.data)
